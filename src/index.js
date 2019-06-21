5ac80a931b80b5751de7aa329880b7066b681fa0
mattfrancis888 <mattfrancis888@gmail.com>
2019-06-21 19:25:59 -0400

    Created wepack config setup. But didn't add any plug-in for optimization.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+import webpackimg from './img/webpack-img.jpg'
+
+const img = document.createElement("img")
+img.src = webpackimg
+document.body.appendChild(img)
+
+let a = 3
+console.log(a);
+let test = () => console.log(123);
+test();
+
+let b = 5
+console.log(b)
